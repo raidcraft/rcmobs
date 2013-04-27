405058e7750fefaaf9afc570fb6d485b4c9185fd
Silthus <silthus@raid-craft.de>
2013-04-27 17:52:56 +0200

    trying the player health thingy again

---
+++ src/main/java/de/raidcraft/mobs/creatures/SkilledCreature.java
@@ -0,0 +1,21 @@
+package de.raidcraft.mobs.creatures;
+
+import de.raidcraft.skills.creature.Creature;
+import org.bukkit.entity.LivingEntity;
+
+/**
+ * @author Silthus
+ */
+public class SkilledCreature extends Creature {
+
+    public SkilledCreature(LivingEntity entity) {
+
+        super(entity);
+    }
+
+    @Override
+    public int getDefaultHealth() {
+
+        return super.getDefaultHealth();
+    }
+}
