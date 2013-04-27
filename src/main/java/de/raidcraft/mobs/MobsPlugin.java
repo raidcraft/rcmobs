617081175d70e5393d118c6b39f7e3d070aa1056
Silthus <silthus@raid-craft.de>
2013-04-27 12:39:49 +0200

    intial commit

---
+++ src/main/java/de/raidcraft/mobs/MobsPlugin.java
@@ -0,0 +1,19 @@
+package de.raidcraft.mobs;
+
+import de.raidcraft.api.BasePlugin;
+
+/**
+ * @author Silthus
+ */
+public class MobsPlugin extends BasePlugin {
+
+    @Override
+    public void enable() {
+        //TODO: implement
+    }
+
+    @Override
+    public void disable() {
+        //TODO: implement
+    }
+}
