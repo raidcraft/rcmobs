97510bae7d1e0214cc4951cbb711e4191e9862ab
Silthus <md.doering@yahoo.de>
2015-02-22 13:29:26 +0100

    fix table mapping

--- src/main/java/de/raidcraft/mobs/tables/TMobGroupSpawnLocation.java
+++ src/main/java/de/raidcraft/mobs/tables/TMobGroupSpawnLocation.java
@@ -1,6 +1,8 @@
 package de.raidcraft.mobs.tables;
 
 import com.avaje.ebean.validation.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
@@ -18,6 +20,8 @@ import java.util.List;
  */
 @Entity
 @Table(name = "rcmobs_mobgroup_spawn_location")
+@Getter
+@Setter
 public class TMobGroupSpawnLocation {
 
     @Id
@@ -32,86 +36,6 @@ public class TMobGroupSpawnLocation {
     private Timestamp lastSpawn;
     @OneToMany(mappedBy = "spawn_group_location_source_id", cascade = CascadeType.REMOVE)
     private List<TSpawnedMobGroup> spawnedMobGroups = new ArrayList<>();
-
-    public int getId() {
-
-        return id;
-    }
-
-    public void setId(int id) {
-
-        this.id = id;
-    }
-
-    public String getSpawnGroup() {
-
-        return spawnGroup;
-    }
-
-    public void setSpawnGroup(String spawnGroup) {
-
-        this.spawnGroup = spawnGroup;
-    }
-
-    public int getX() {
-
-        return x;
-    }
-
-    public void setX(int x) {
-
-        this.x = x;
-    }
-
-    public int getY() {
-
-        return y;
-    }
-
-    public void setY(int y) {
-
-        this.y = y;
-    }
-
-    public int getZ() {
-
-        return z;
-    }
-
-    public void setZ(int z) {
-
-        this.z = z;
-    }
-
-    public String getWorld() {
-
-        return world;
-    }
-
-    public void setWorld(String world) {
-
-        this.world = world;
-    }
-
-    public double getCooldown() {
-
-        return cooldown;
-    }
-
-    public void setCooldown(double cooldown) {
-
-        this.cooldown = cooldown;
-    }
-
-    public Timestamp getLastSpawn() {
-
-        return lastSpawn;
-    }
-
-    public void setLastSpawn(Timestamp lastSpawn) {
-
-        this.lastSpawn = lastSpawn;
-    }
 
     public Location getBukkitLocation() {
         // TODO: cache it
