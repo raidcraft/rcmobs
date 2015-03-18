c1238193f8afd82b8d0d1cb7642d429a9d871ad8
Silthus <md.doering@yahoo.de>
2015-03-18 17:34:46 +0100

    maybe fixed duplicate uuid

--- src/main/java/de/raidcraft/mobs/groups/ConfigurableMobGroup.java
+++ src/main/java/de/raidcraft/mobs/groups/ConfigurableMobGroup.java
@@ -154,7 +154,7 @@ public class ConfigurableMobGroup extends AbstractSpawnable implements MobGroup 
             TSpawnedMob spawnedMob = db.find(TSpawnedMob.class).where().eq("uuid", mob.getEntity().getUniqueId()).findUnique();
             if (spawnedMob != null) {
                 spawnedMob.setMobGroupSource(spawnedMobGroup);
-                db.save(spawnedMob);
+                db.update(spawnedMob);
             }
         }
         return spawnedMobs;
